ab34cff2159212d9810d690d7a48aed5f9207e34
tharusha dilhara jayadeera <edu.tharusha@gmail.com>
2022-11-05 08:30:31 +0530

    aedit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Cyborg - Awesome HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <!--
+
+    TemplateMo 579 Cyborg Gaming
+
+    https://templatemo.com/tm-579-cyborg-gaming
+
+    -->
+</head>
+
+<body>
+
+
+<!-- ***** Preloader End ***** -->
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="page-content">
+
+                <!-- ***** Banner Start ***** -->
+                <div class="main-banner">
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="header-text">
+                                <h6>Welcome To LMS</h6>
+                                <h6>LMS වෙත සාදරයෙන් පිළිගනිමි.</h6>
+                                <h4><em>Browse</em> your subject</h4>
+                                <h6>ඔබගේ විශය තෝරන්න</h6>
+                                <div class="main-button">
+                                    <a href="{{route('login')}}">Login Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ***** Banner End ***** -->
+
+                <!-- ***** Most Popular Start ***** -->
+                <div class="most-popular">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="heading-section">
+                                <h4><em>Most Popular</em> Right Now</h4>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-01.jpg" alt="">
+                                        <h4>6 වසර - ගණිතය<br><span>කසුන් සමර</span></h4>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-02.jpg" alt="">
+                                        <h4>8 වසර ඉතිහාසය<br><span>නවෝද මදුවන්ත</span></h4>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-03.jpg" alt="">
+                                        <h4>8 වසර - ගණිතය<br><span>TD ජයදීර</span></h4>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-04.jpg" alt="">
+                                        <h4>11 වසර - සිංහල<br><span>බුද්දිනී ගනරත්න</span></h4>
+
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-07.jpg" alt="">
+                                        <h4>11 වසර - ගනිතය<br><span>සුපුන් මදුවන්ත</span></h4>
+
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-07.jpg" alt="">
+                                        <h4>11 වසර - ඉතිහාසය<br><span>දිල්ශාන් උඩවත්ත</span></h4>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-sm-6">
+                                    <div class="item">
+                                        <img src="assets/images/popular-08.jpg" alt="">
+                                        <h4>8 වසර - ICT<br><span>පුපුන් මදුසශංක</span></h4>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="main-button">
+                                        <a href="browse.html">Discover Popular</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ***** Most Popular End ***** -->
+
+{{--                <!-- ***** Gaming Library Start ***** -->--}}
+{{--                <div class="gaming-library">--}}
+{{--                    <div class="col-lg-12">--}}
+{{--                        <div class="heading-section">--}}
+{{--                            <h4><em>Your Gaming</em> Library</h4>--}}
+{{--                        </div>--}}
+{{--                        <div class="item">--}}
+{{--                            <ul>--}}
+{{--                                <li><img src="assets/images/game-01.jpg" alt="" class="templatemo-item"></li>--}}
+{{--                                <li><h4>Dota 2</h4><span>Sandbox</span></li>--}}
+{{--                                <li><h4>Date Added</h4><span>24/08/2036</span></li>--}}
+{{--                                <li><h4>Hours Played</h4><span>634 H 22 Mins</span></li>--}}
+{{--                                <li><h4>Currently</h4><span>Downloaded</span></li>--}}
+{{--                                <li><div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                        <div class="item">--}}
+{{--                            <ul>--}}
+{{--                                <li><img src="assets/images/game-02.jpg" alt="" class="templatemo-item"></li>--}}
+{{--                                <li><h4>Fortnite</h4><span>Sandbox</span></li>--}}
+{{--                                <li><h4>Date Added</h4><span>22/06/2036</span></li>--}}
+{{--                                <li><h4>Hours Played</h4><span>740 H 52 Mins</span></li>--}}
+{{--                                <li><h4>Currently</h4><span>Downloaded</span></li>--}}
+{{--                                <li><div class="main-border-button"><a href="#">Donwload</a></div></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                        <div class="item last-item">--}}
+{{--                            <ul>--}}
+{{--                                <li><img src="assets/images/game-03.jpg" alt="" class="templatemo-item"></li>--}}
+{{--                                <li><h4>CS-GO</h4><span>Sandbox</span></li>--}}
+{{--                                <li><h4>Date Added</h4><span>21/04/2036</span></li>--}}
+{{--                                <li><h4>Hours Played</h4><span>892 H 14 Mins</span></li>--}}
+{{--                                <li><h4>Currently</h4><span>Downloaded</span></li>--}}
+{{--                                <li><div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-lg-12">--}}
+{{--                        <div class="main-button">--}}
+{{--                            <a href="profile.html">View Your Library</a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- ***** Gaming Library End ***** -->--}}
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright © 2036 <a href="#">බොඩිම</a> Company. All rights reserved.
+
+                    <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>  Distributed By <a href="#" target="_blank" >Tharusha</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="assets/js/isotope.min.js"></script>
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/tabs.js"></script>
+<script src="assets/js/popup.js"></script>
+<script src="assets/js/custom.js"></script>
+
+
+</body>
+
+</html>
